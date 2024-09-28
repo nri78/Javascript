059527beca670574fc246334ba40055107aac0c5
nri78 <navin.rijhwani@gmail.com>
2024-09-28 14:34:59 +0000

    container config changed

---
+++ 01_Variables/test.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "navin.rijhwani@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mysore"
+let accountState
+
+//accountId = 2 (not allowed)
+
+accountEmail = "abc.test.com"
+accountPassword = "c@u8310n"
+accountCity = "Bombay"
+
+/*
+Prefer not to use 'var'
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
